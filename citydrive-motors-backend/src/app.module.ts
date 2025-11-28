343a4651fa76acdb325744b39cafe6c7d5483c96
Hammad Bin Tariq <124447376+Hammad-Tariq007@users.noreply.github.com>
2025-11-28 11:54:54 +0500

    Add AppController and AppService to AppModule

--- citydrive-motors-backend/src/app.module.ts
+++ citydrive-motors-backend/src/app.module.ts
@@ -4,6 +4,8 @@ import { typeormConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
 import { CarsModule } from './cars/cars.module';
 import { RemarksModule } from './remarks/remarks.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -12,5 +14,7 @@ import { RemarksModule } from './remarks/remarks.module';
     CarsModule,
     RemarksModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
